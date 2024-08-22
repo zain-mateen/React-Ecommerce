9cd6cac16efddfa4c70f9a49e03d206f749b49cd
Zain Mateen <zaini_2223@yahoo.com>
2024-08-23 00:31:08 +0500

    FIX THE FIREBASE

--- src/utils/Firebase/Firebase.utils.js
+++ src/utils/Firebase/Firebase.utils.js
@@ -30,6 +30,8 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
+console.log(firebaseApp);
+
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
     prompt: "select_account",
